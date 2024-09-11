d6ad126a3c0b761b822abdc519a28439a71a21db
akthakur04 <akshay.181004@gmail.com>
2024-09-12 01:19:03 +0530

    style

--- app/page.js
+++ app/page.js
@@ -1,23 +1,74 @@
 'use client';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import {
+  Container,
+  Typography,
+  Button,
+  Box,
+} from '@mui/material';
 
-export default function Home() {
-  const [items, setItems] = useState([]);
-  function fetchData() {
-    fetch('/api/todo').then(response => response.json()).then((data) => { setItems(data) });
-  }
+export default function HomePage() {
+  const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   // Fetch the user from localStorage
+  //   const userData = localStorage.getItem('user');
+  //   if (userData) {
+  //     setUser(JSON.parse(userData));
+  //   }
+  // }, []);
+
+
   useEffect(() => {
-    fetchData();
+    if (typeof window !== 'undefined') {
+      const userData = localStorage.getItem('user');
+      if (userData) {
+        setUser(JSON.parse(userData));
+      }
+    }
   }, []);
-  console.log(items)
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    window.location.reload();
+  };
+
   return (
-    <div>
-      <h1>Tasks</h1>
-      <ul>
-        {items && items?.map((item) => (
-          <li key={item._id}>{item.title} -  {item.description}</li>  
-        ))}
-      </ul>
-    </div>
+    <Container maxWidth="md" style={{ padding: '20px' }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Welcome to the Next.js CRUD App
+      </Typography>
+
+      <Box mt={3}>
+        <Typography variant="h6" component="p">
+          Welcome,
+           {/* {user.email} */}
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          // onClick={handleLogout}
+          style={{ margin: '20px 0' }}
+        >
+          Logout
+        </Button>
+        <Box>
+          {/* <Link href="/tasks/create" passHref> */}
+            <Button variant="contained" color="primary" fullWidth>
+              Create a Task
+            </Button>
+          {/* </Link> */}
+          <Box mt={2}>
+            <Link href="/tasks" passHref>
+              <Button variant="outlined" color="primary" fullWidth>
+                View Tasks
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+
+    </Container>
   );
 }
